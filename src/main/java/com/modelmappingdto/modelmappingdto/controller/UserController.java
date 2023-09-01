77ac51c9c5bf3830ca12f4d53c93e434ec20cec4
SAMBATH Theara <theara.sambath@allweb.com.kh>
2023-09-01 14:36:05 +0700

    New

--- src/main/java/com/modelmappingdto/modelmappingdto/controller/UserController.java
+++ src/main/java/com/modelmappingdto/modelmappingdto/controller/UserController.java
@@ -2,18 +2,20 @@ package com.modelmappingdto.modelmappingdto.controller;
 
 import com.modelmappingdto.modelmappingdto.dto.UserLocationDTO;
 import com.modelmappingdto.modelmappingdto.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 public class UserController {
     @Autowired
     private UserService userService;
     @GetMapping("/getUser")
     private List<UserLocationDTO> getAllUserLocation(){
+        log.debug("--------Successful get the user information----------");
         return userService.getAllUserLocation();
     }
 }
